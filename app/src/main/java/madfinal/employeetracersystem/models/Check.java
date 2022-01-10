1ce74390cd1bb28843aaf498ecaccad7f625c5e2
zehra <ridvanogullarizehra@gmail.com>
2022-01-10 21:02:59 +0300

    added Checks model

---
+++ app/src/main/java/madfinal/employeetracersystem/models/Check.java
@@ -0,0 +1,10 @@
+package madfinal.employeetracersystem.models;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class Check {
+    public int userSSN;
+    public Timestamp chekinTime;
+    public Timestamp checkoutTime;
+}
---
+++ app/src/main/java/madfinal/employeetracersystem/models/Checks.java
@@ -0,0 +1,8 @@
+package madfinal.employeetracersystem.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Checks {
+    private List<Check> checks = new ArrayList<>();
+}
